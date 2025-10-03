218cad1e2d6755a5f278f8c3e9f6f1a08ee7198a
ajeet kumar <ajeetk8568@gmail.com>
2025-10-03 15:47:49 +0530

    create getStudentById

--- src/main/java/com/example/springbootRestAPI/controller/StudentController.java
+++ src/main/java/com/example/springbootRestAPI/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.example.springbootRestAPI.controller;
 import com.example.springbootRestAPI.dto.StudentDto;
 import com.example.springbootRestAPI.service.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,10 @@ public class StudentController {
     public List<StudentDto> getAllStudents(){
         System.out.println("ka hal chal");
         return studentService.getAllStudents();
+    }
+
+    @GetMapping("/students/{id}")
+    public StudentDto getStudentById(@PathVariable Long id){
+       return studentService.getStudentById(id);
     }
 }
--- src/main/java/com/example/springbootRestAPI/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/springbootRestAPI/service/impl/StudentServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.springbootRestAPI.entity.Student;
 import com.example.springbootRestAPI.repository.StudentRepository;
 import com.example.springbootRestAPI.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,10 +14,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class StudentServiceImpl implements StudentService {
     private final StudentRepository studentRepository;
+    private final ModelMapper modelMapper;
     public List<StudentDto> getAllStudents(){
          List<Student> students = studentRepository.findAll();
          List<StudentDto> studentDtoList = students.stream().
                   map(student->new StudentDto(student.getId(), student.getName(), student.getEmail(), student.getBranch())).toList();
          return studentDtoList;
+    }
+
+    public StudentDto getStudentById(Long id){
+        Student st = studentRepository.getReferenceById(id);
+        StudentDto student = modelMapper.map(st, StudentDto.class);
+        return student;
     }
 }
